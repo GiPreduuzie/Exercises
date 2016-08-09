f34a72c314927c996ef4fa937f151bf77869b2cb
GiPreduuzie <anavator@yandex.ru>
2016-08-09 16:35:35 +0300

    Split tests

--- Queue/QueueTests/QueueTests.cs
+++ Queue/QueueTests/QueueTests.cs
@@ -11,28 +11,24 @@ namespace QueueTests
         }
 
         [TestMethod]
-        public void TestMethod1()
-        {
-            GeneralCase();
-        }
-
-        private void SimpleCase()
+        public void SimpleCase()
         {
             var results = GetQueueUsers<int>()
-               .Thread(1).Push(1)
-               .Thread(1).Push(2)
-               .Thread(1).Push(3)
-               .Thread(2).Pop()
-               .Thread(2).Pop()
-               .Thread(2).Pop()
-               .Run();
+                .Thread(1).Push(1)
+                .Thread(1).Push(2)
+                .Thread(1).Push(3)
+                .Thread(2).Pop()
+                .Thread(2).Pop()
+                .Thread(2).Pop()
+                .Run();
 
             CollectionAssert.AreEqual(new int[0], results.GetResultOfThread(1));
             CollectionAssert.AreEqual(new int[] { 1, 2, 3 }, results.GetResultOfThread(2));
             CollectionAssert.AreEqual(new int[0], results.GetQueueState());
         }
 
-        private void GeneralCase()
+        [TestMethod]
+        public void GeneralCase()
         {
             var results = GetQueueUsers<int>()
                .Thread(2).Pop()
@@ -47,9 +43,9 @@ namespace QueueTests
                .Thread(1).Push(6)
                .Run();
 
-            CollectionAssert.AreEqual(new [] { 3, 4}, results.GetResultOfThread(1));
-            CollectionAssert.AreEqual(new [] { 1, 2 }, results.GetResultOfThread(2));
-            CollectionAssert.AreEqual(new [] { 5, 6 }, results.GetQueueState());
+            CollectionAssert.AreEqual(new[] { 3, 4 }, results.GetResultOfThread(1));
+            CollectionAssert.AreEqual(new[] { 1, 2 }, results.GetResultOfThread(2));
+            CollectionAssert.AreEqual(new[] { 5, 6 }, results.GetQueueState());
         }
     }
 }
